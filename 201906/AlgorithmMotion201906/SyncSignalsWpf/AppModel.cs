984a4ee9a368cfb82b4b1b517ba8885661d8fd4e
Keiho Sakapon <git@saka-pon.net>
2019-06-28 15:58:29 +0900

    Add methods

--- 201906/AlgorithmMotion201906/SyncSignalsWpf/AppModel.cs
+++ 201906/AlgorithmMotion201906/SyncSignalsWpf/AppModel.cs
@@ -16,19 +16,56 @@ namespace SyncSignalsWpf
         public PointObject[] Points { get; }
 
         OrderedList<PointObject, DateTime> SignalTimes;
+        OrderedList<PointObject, DateTime> ThinkingTimes;
         Timer FrameTimer;
 
         public AppModel()
         {
+            var now = DateTime.Now;
             var angleDelta = 360.0 / PointsCount;
             Points = Enumerable.Range(0, PointsCount)
-                .Select(i => new PointObject(i, i * angleDelta))
+                .Select(_ => now.AddSeconds(2 + SignalInterval * Random.NextDouble()))
+                .Select((t, i) => new PointObject(i, i * angleDelta)
+                {
+                    NextSignalTime = t,
+                    NextThinkingTime = t.AddSeconds(SignalInterval / 2),
+                })
                 .ToArray();
+
+            SignalTimes = new OrderedList<PointObject, DateTime>(p => p.NextSignalTime);
+            ThinkingTimes = new OrderedList<PointObject, DateTime>(p => p.NextThinkingTime);
 
             FrameTimer = new Timer(UpdateFrame, null, TimeSpan.Zero, TimeSpan.FromSeconds(1 / Fps));
         }
 
         void UpdateFrame(object state)
+        {
+            var now = DateTime.Now;
+
+            var countToThink = ThinkingTimes.TakeWhile(_ => _.key < now).Count();
+            for (var i = 0; i < countToThink; i++)
+            {
+                var point = ThinkingTimes[0].item;
+                ThinkingTimes.RemoveAt(0);
+
+                UpdateThinking(point);
+            }
+
+            var countToSignal = SignalTimes.TakeWhile(_ => _.key < now).Count();
+            for (var i = 0; i < countToSignal; i++)
+            {
+                var point = SignalTimes[0].item;
+                SignalTimes.RemoveAt(0);
+
+                UpdateSignal(point);
+            }
+        }
+
+        void UpdateThinking(PointObject p)
+        {
+        }
+
+        void UpdateSignal(PointObject p)
         {
         }
     }
