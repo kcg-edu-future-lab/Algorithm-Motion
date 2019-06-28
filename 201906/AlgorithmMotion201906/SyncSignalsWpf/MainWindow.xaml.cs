f25db2ace5af3a7291d33cb5d88d339834dc3b17
Keiho Sakapon <git@saka-pon.net>
2019-06-28 23:35:28 +0900

    Add wink animation

--- 201906/AlgorithmMotion201906/SyncSignalsWpf/MainWindow.xaml.cs
+++ 201906/AlgorithmMotion201906/SyncSignalsWpf/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,38 @@ namespace SyncSignalsWpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void SignalCircle_Loaded(object sender, RoutedEventArgs e)
+        {
+            var ellipse = (Ellipse)sender;
+            var point = (PointObject)ellipse.DataContext;
+
+            var wink = CreateWinkAnimation(ellipse);
+
+            point.PropertyChanged += (o, pe) =>
+            {
+                if (pe.PropertyName != nameof(point.PreviousSignalTime)) return;
+
+                Dispatcher.InvokeAsync(() =>
+                {
+                    wink.Begin(ellipse);
+                });
+            };
+        }
+
+        static Storyboard CreateWinkAnimation(FrameworkElement element, double opacity = 1, double time = 0.6)
+        {
+            var storyboard = new Storyboard();
+            var frames = new DoubleAnimationUsingKeyFrames();
+
+            Storyboard.SetTarget(frames, element);
+            Storyboard.SetTargetProperty(frames, new PropertyPath(UIElement.OpacityProperty.Name));
+            frames.KeyFrames.Add(new EasingDoubleKeyFrame(opacity, TimeSpan.Zero, new CubicEase { EasingMode = EasingMode.EaseOut }));
+            frames.KeyFrames.Add(new EasingDoubleKeyFrame(0, TimeSpan.FromSeconds(time), new CubicEase { EasingMode = EasingMode.EaseOut }));
+            storyboard.Children.Add(frames);
+
+            return storyboard;
         }
     }
 }
