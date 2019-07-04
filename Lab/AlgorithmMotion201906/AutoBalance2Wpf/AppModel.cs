52890b4c119bb22a76a9f591561e567e6c9c7a2c
Keiho Sakapon <git@saka-pon.net>
2019-07-04 17:24:50 +0900

    Add classes

---
+++ Lab/AlgorithmMotion201906/AutoBalance2Wpf/AppModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace AutoBalance2Wpf
+{
+    public class AppModel : NotifyBase
+    {
+        const int PointsCount = 24;
+        const double MoveInterval = 1.0;
+        const double Fps = 30;
+
+        internal static Random Random { get; } = new Random();
+
+        public PointObject[] Points { get; }
+        Queue<(DateTime time, PointObject point)> TimeQueue;
+
+        Timer FrameTimer;
+        Stopwatch FrameWatch = new Stopwatch();
+
+        double _ActualFrameTime;
+
+        public double ActualFrameTime
+        {
+            get { return _ActualFrameTime; }
+            set
+            {
+                if (_ActualFrameTime == value) return;
+                _ActualFrameTime = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public AppModel()
+        {
+            Points = Enumerable.Range(0, PointsCount)
+                .Select(i => new PointObject(i, new Vector(-300 + 600 * Random.NextDouble(), -300 + 600 * Random.NextDouble())))
+                .ToArray();
+
+            var now = DateTime.Now;
+            var queueQuery = Points
+                .Select(o => (time: now.AddSeconds(2 + MoveInterval * Random.NextDouble()), point: o))
+                .OrderBy(_ => _.time);
+            TimeQueue = new Queue<(DateTime, PointObject)>(queueQuery);
+
+            FrameTimer = new Timer(UpdateFrame, null, TimeSpan.Zero, TimeSpan.FromSeconds(1 / Fps));
+        }
+
+        void UpdateFrame(object state)
+        {
+            FrameWatch.Restart();
+            var now = DateTime.Now;
+
+            var countToUpdate = TimeQueue.TakeWhile(_ => _.time < now).Count();
+            for (var i = 0; i < countToUpdate; i++)
+            {
+                var (time, point) = TimeQueue.Dequeue();
+                TimeQueue.Enqueue((time.AddSeconds(MoveInterval), point));
+
+                UpdatePoint(point);
+            }
+
+            FrameWatch.Stop();
+            ActualFrameTime = FrameWatch.Elapsed.TotalMilliseconds;
+        }
+
+        void UpdatePoint(PointObject p)
+        {
+            var radius = new[] { -1, 1 }
+                .Average(i => Points[(p.Id + i).Mod(PointsCount)].Position.Length);
+            var angle = new[] { -1, 1 }
+                .Select(i => Points[(p.Id + i).Mod(PointsCount)].Angle)
+                .Average(a => a > p.Angle + 180 ? a - 360 : a < p.Angle - 180 ? a + 360 : a);
+
+            p.Position = new Vector(radius * Math.Cos(angle * Math.PI / 180), radius * Math.Sin(angle * Math.PI / 180));
+        }
+    }
+}
---
+++ Lab/AlgorithmMotion201906/AutoBalance2Wpf/PointObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace AutoBalance2Wpf
+{
+    public abstract class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public class PointObject : NotifyBase
+    {
+        static readonly Vector UnitX = new Vector(1, 0);
+
+        public int Id { get; }
+
+        Vector _Position;
+
+        public Vector Position
+        {
+            get { return _Position; }
+            set
+            {
+                if (_Position == value) return;
+                _Position = value;
+                Angle = Vector.AngleBetween(UnitX, value);
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double Angle { get; private set; }
+
+        public PointObject(int id, Vector position)
+        {
+            Id = id;
+            _Position = position;
+        }
+
+        public override string ToString() => $"{Id}: {Position}";
+    }
+
+    public static class NumberHelper
+    {
+        public static int Mod(this int i, int n)
+        {
+            var j = i % n;
+            return j >= 0 ? j : j + n;
+        }
+    }
+}
