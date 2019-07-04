ab7f2a1036bba3f44f3caa9a5c8790674ffda54e
Keiho Sakapon <git@saka-pon.net>
2019-07-04 18:40:26 +0900

    Update layout

--- Lab/AlgorithmMotion201906/AutoBalance2Wpf/MainWindow.xaml.cs
+++ Lab/AlgorithmMotion201906/AutoBalance2Wpf/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,50 @@ namespace AutoBalance2Wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void Circle_Loaded(object sender, RoutedEventArgs e)
+        {
+            var ellipse = (Ellipse)sender;
+            var point = (PointObject)ellipse.DataContext;
+
+            var (move, _, _) = CreateMoveAnimation("PositionTransform", point.Position.X, point.Position.Y, 0.5 + AppModel.Random.NextDouble());
+            move.Begin(ellipse);
+
+            var (moveForChange, frameX, frameY) = CreateMoveAnimation("PositionTransform");
+
+            point.PropertyChanged += (o, pe) =>
+            {
+                if (pe.PropertyName != nameof(point.Position)) return;
+
+                Dispatcher.InvokeAsync(() =>
+                {
+                    frameX.Value = point.Position.X;
+                    frameY.Value = point.Position.Y;
+                    moveForChange.Begin(ellipse);
+                });
+            };
+        }
+
+        static (Storyboard, DoubleKeyFrame, DoubleKeyFrame) CreateMoveAnimation(string translateTransformName, double x = 0, double y = 0, double time = 0.4)
+        {
+            var storyboard = new Storyboard();
+
+            var framesX = new DoubleAnimationUsingKeyFrames();
+            var frameX = new EasingDoubleKeyFrame(x, TimeSpan.FromSeconds(time), new CubicEase { EasingMode = EasingMode.EaseOut });
+            Storyboard.SetTargetName(framesX, translateTransformName);
+            Storyboard.SetTargetProperty(framesX, new PropertyPath(TranslateTransform.XProperty.Name));
+            framesX.KeyFrames.Add(frameX);
+            storyboard.Children.Add(framesX);
+
+            var framesY = new DoubleAnimationUsingKeyFrames();
+            var frameY = new EasingDoubleKeyFrame(y, TimeSpan.FromSeconds(time), new CubicEase { EasingMode = EasingMode.EaseOut });
+            Storyboard.SetTargetName(framesY, translateTransformName);
+            Storyboard.SetTargetProperty(framesY, new PropertyPath(TranslateTransform.YProperty.Name));
+            framesY.KeyFrames.Add(frameY);
+            storyboard.Children.Add(framesY);
+
+            return (storyboard, frameX, frameY);
         }
     }
 }
