6ad34f8e9b2a651f3235e4032b681a6bad601c2f
Keiho Sakapon <git@saka-pon.net>
2019-07-07 17:16:25 +0900

    Add method for random

--- Lab/AlgorithmMotion201906/AutoBalance2Wpf/PointObject.cs
+++ Lab/AlgorithmMotion201906/AutoBalance2Wpf/PointObject.cs
@@ -48,6 +48,13 @@ namespace AutoBalance2Wpf
 
     public static class NumberHelper
     {
+        internal static Random Random { get; } = new Random();
+
+        public static double NextDouble(double minValue, double maxValue)
+        {
+            return minValue + (maxValue - minValue) * Random.NextDouble();
+        }
+
         public static int Mod(this int i, int n)
         {
             var j = i % n;
--- Lab/AlgorithmMotion201906/SyncSignalsWpf/PointObject.cs
+++ Lab/AlgorithmMotion201906/SyncSignalsWpf/PointObject.cs
@@ -44,6 +44,13 @@ namespace SyncSignalsWpf
 
     public static class NumberHelper
     {
+        internal static Random Random { get; } = new Random();
+
+        public static double NextDouble(double minValue, double maxValue)
+        {
+            return minValue + (maxValue - minValue) * Random.NextDouble();
+        }
+
         public static int Mod(this int i, int n)
         {
             var j = i % n;
